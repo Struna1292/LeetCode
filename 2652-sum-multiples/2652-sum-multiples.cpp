e2c3291f77a991cffbc7de7dc16d4039a47b78e4
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2023-05-13 20:25:49 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (54.60%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        
+        int output = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+            {
+                output += i;
+            }
+        }
+
+        return output;
+    }
+};
