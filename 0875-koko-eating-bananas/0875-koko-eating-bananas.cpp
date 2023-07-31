d63b187679c1bd9ef6d9146887a10c6d095ab334
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2023-07-31 18:43:54 +0200

    Time: 50 ms (69.14%), Space: 19 MB (14.87%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        sort(begin(piles), end(piles));
+
+        int left = 1;
+        int right = piles[piles.size()-1];
+
+        int answer = -1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            int currHours = h;
+            for (int i = 0; i < piles.size(); i++)
+            {
+                currHours -= ceil((double)piles[i] / (double)mid);
+            }
+            
+            if (currHours >= 0)
+            {
+                answer = mid;
+                right = mid - 1;             
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return answer;
+    }
+};
