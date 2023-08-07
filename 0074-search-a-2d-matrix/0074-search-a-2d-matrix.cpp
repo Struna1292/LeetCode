32c9b88b43d2e32a1093c11f188ce419a5ae3994
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2023-08-07 16:46:22 +0200

    Time: 3 ms (82.89%), Space: 9.6 MB (19.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,18 +2,54 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
-        for (int i = 0; i < matrix.size(); i++)
+        int left = 0;
+        int right = matrix.size()-1;
+        int row = -1;
+
+        while (left <= right)
         {
-            if (matrix[i][matrix[i].size()-1] >= target)
+            int mid = left + (right - left) / 2;
+ 
+            if (target >= matrix[mid][0] && target <= matrix[mid][matrix[mid].size()-1])
             {
-                for (int j = 0; j < matrix[i].size(); j++)
-                {
-                    if (matrix[i][j] == target)
-                    {
-                        return true;
-                    }
-                }
-                return false;
+                row = mid;
+                break;
+            }
+
+            if (target < matrix[mid][0])
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        if (row == -1)
+        {
+            return false;
+        }
+
+        left = 0;
+        right = matrix[row].size()-1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (target == matrix[row][mid])
+            {
+                return true;
+            }
+
+            if (matrix[row][mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
             }
         }
 
