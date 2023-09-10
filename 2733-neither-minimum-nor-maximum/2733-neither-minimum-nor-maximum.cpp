cbde9d935bd049f3ba9f8cbc8f21d3573dd501e2
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2023-09-10 12:36:28 +0200

    Time: 89 ms (76.12%), Space: 91.7 MB (74.92%) - LeetHub

---
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) {
+        if (nums.size() <= 2)
+        {
+            return -1;
+        }
+
+        sort(nums.begin(), nums.end());
+
+        return nums[1];
+    }
+};
