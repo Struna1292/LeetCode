3f50786e9e981c0c4802f08d690cb47796ca6368
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2023-09-18 10:28:12 +0200

    Time: 14 ms (14.22%), Space: 10.8 MB (13.97%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        vector<pair<int,int>> soldiers;
+        vector<pair<int,int>> rows; // value and index
 
         for (int i = 0; i < mat.size(); i++)
         {
-            int counter = 0;
+            int value = 0;
             for (int j = 0; j < mat[i].size(); j++)
             {
-                if (mat[i][j] == 0)
+                if (mat[i][j] == 1)
                 {
-                    break;
+                    value++;
                 }
-                counter++;
             }
-            soldiers.push_back({counter, i});
+            rows.push_back({value,i});
         }
 
-        sort(begin(soldiers), end(soldiers));
+        sort(begin(rows), end(rows));
 
         vector<int> output;
 
         for (int i = 0; i < k; i++)
         {
-            output.push_back(soldiers[i].second);
+            output.push_back(rows[i].second);
         }
 
         return output;
