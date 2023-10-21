08f7b8944b5accfb4e958777e8f965038b5961ea
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2023-10-21 14:48:21 +0200

    Time: 2 ms (47.70%), Space: 6.5 MB (36.93%) - LeetHub

---
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+
+        if (a == b)
+        {
+            return -1;
+        }
+
+        if (a.length() > b.length())
+        {
+            return a.length();
+        }
+        return b.length();
+    }
+};
