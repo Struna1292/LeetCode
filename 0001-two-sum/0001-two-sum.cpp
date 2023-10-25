2161234ecedbddeb769d38a03ddb30a00be1d11b
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2023-10-25 20:23:11 +0200

    Time: 274 ms (34.49%), Space: 10.8 MB (51.83%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> output(2, 0);
+
+        for (int i = 0; i < nums.size()-1; i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        
+        return output;
+    }
+};
