3065f35043fb598f355700a9e8d74b0a5cc16bf6
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2023-11-19 14:01:29 +0100

    Time: 146 ms (90.80%), Space: 83 MB (98.16%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
-
+        
         sort(begin(nums), end(nums));
 
-        int power = 0;
-        int output = 0;
-        int counter = 1;
         int prev = nums[0];
+        int output = 0;
+        int power = 0;
+
         for (int i = 1; i < nums.size(); i++)
         {
             if (nums[i] != prev)
             {
-                output += (counter * power);
-                prev = nums[i];
-                counter = 1;
                 power++;
+                prev = nums[i];
             }
-            else
-            {
-                counter++;
-            }
+            output += power;
         }
-        output += (counter * power);
 
         return output;
     }
