8af6490b4c5d4856e0bc59175d2eaca5d68e2ac6
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2023-11-30 12:32:10 +0100

    Time: 4 ms (10.90%), Space: 6.2 MB (80.09%) - LeetHub

--- 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -2,41 +2,46 @@ class Solution {
 public:
     int minimumOneBitOperations(int n) {
 
-        string s;
+        int sum1 = 0;
+        int sum2 = 0;
+        int i = 0;
+        bool check = true;
+
+        int output = 0;
 
         while (n > 0)
         {
             if ((n & 1) == 1)
             {
-                s.push_back('1');
-            }
-            else
-            {
-                s.push_back('0');
-            }
-            n >>= 1;
-        }
-
-        int output = 0;
-        bool plus = true;
-
-        for (int i = s.length()-1; i >= 0; i--)
-        {
-            if (s[i] == '1')
-            {
-                if (plus)
+                if (check)
                 {
-                    output += pow(2,i+1)-1;
-                    plus = false;
+                    sum1 += pow(2,i+1)-1;
+                    check = false;
                 }
                 else
                 {
-                    output -= pow(2,i+1)-1;
-                    plus = true;
+                    sum2 += pow(2,i+1)-1;
+                    check = true;
+                } 
+            }
+
+
+            n >>= 1;
+            if (n == 0)
+            {
+                if (sum1 > sum2)
+                {
+                    output = sum1 - sum2;
+                }
+                else
+                {
+                    output = sum2 - sum1;
                 }
             }
+            i++;
         }
 
+        
         return output;
     }
 };
