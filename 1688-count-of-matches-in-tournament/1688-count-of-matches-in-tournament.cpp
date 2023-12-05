e34219081de16b9ed73e08cb99b4ba2637ca4bc1
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2023-12-05 18:46:03 +0100

    Time: 3 ms (17.67%), Space: 6.2 MB (73.09%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        int matchSum = 0;
+
+        int output = 0;
+
         while (n > 1)
         {
             if (n % 2 != 0)
             {
                 n--;
                 n /= 2;
-                matchSum += n;
+                output += n;
                 n++;
             }
             else
             {
                 n /= 2;
-                matchSum += n;
+                output += n;
             }
         }
-        return matchSum;
+
+        return output;
     }
 };
