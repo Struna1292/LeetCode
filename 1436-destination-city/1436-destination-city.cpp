eb17b2ee95eaf430afea2e4b27b11ca69c1ecc45
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2023-12-15 13:33:15 +0100

    Time: 14 ms (19.38%), Space: 10.5 MB (93.90%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -2,33 +2,27 @@ class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
         
-        string curr = paths[0][1];
-        int index = 0;
+        string output = paths[0][1];
+
         while (true)
         {
-            int counter = 0;
-            for (int i = 1; i < paths.size(); i++)
+            bool found = false;
+            for (int i = 0; i < paths.size(); i++)
             {
-                if (paths[i][0] == curr && index != i)
+                if (output == paths[i][0])
                 {
-                    curr = paths[i][1];
-                    index = i;
-                    counter = 0;
+                    output = paths[i][1];
+                    found = true;
                     break;
                 }
-                else
-                {
-                    counter++;
-                }
             }
-            
-            if (counter+1 == paths.size())
+
+            if (!found)
             {
                 break;
             }
         }
 
-
-        return curr;
+        return output;
     }
 };
