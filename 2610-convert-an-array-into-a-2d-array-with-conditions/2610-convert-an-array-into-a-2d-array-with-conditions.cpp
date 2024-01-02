2e1b94f211f1b2f47c696533e47ee4e386e27937
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2024-01-02 14:36:10 +0100

    Time: 12 ms (56.87%), Space: 29.9 MB (70.96%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,54 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        vector<vector<int>> output;
-        vector<pair<int,int>> occurences;
-        int counter = 0;
+        vector<int> count(nums.size(), 0);
+
         for (int i = 0; i < nums.size(); i++)
         {
-            counter = 0;
-            for (int j = 0; j < occurences.size(); j++)
+            count[nums[i]-1]++;
+        }
+
+        vector<vector<int>> output;
+
+        for (int i = 0; i < count.size(); i++)
+        {
+            for (int j = 0; j < count[i]; j++)
             {
-                if (occurences[j].first == nums[i])
+                if (output.size() < j+1)
                 {
-                    occurences[j].second++;
-                    break;
+                    output.push_back({});
                 }
-                else
-                {
-                    counter++;
-                }
-            }
-            if (counter == occurences.size())
-            {
-                occurences.push_back({nums[i],1});
+                output[j].push_back(i+1);
             }
         }
 
-        counter = 0;
-        vector<int> curr;
-        vector<int> empty;
-        while (counter < occurences.size())
-        {
-            counter = 0;
-            for (int i = 0; i < occurences.size(); i++)
-            {
-                if (occurences[i].second == 0)
-                {
-                    counter++;
-                }
-                else
-                {
-                    occurences[i].second--;
-                    curr.push_back(occurences[i].first);
-                }
-            }
-            if (curr != empty)
-            {
-                output.push_back(curr);
-                curr = empty;
-            }
-        }
         return output;
     }
 };
