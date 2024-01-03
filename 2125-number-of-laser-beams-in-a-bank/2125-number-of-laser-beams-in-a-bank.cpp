d168b17f62fce359a068583074ca0402e6cc0acb
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2024-01-03 14:27:44 +0100

    Time: 110 ms (17.03%), Space: 23.3 MB (82.87%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int counter1 = 0;
-        int counter2 = 0;
-        int lasers = 0;
+        
+        int output = 0;
+
+        int prevLasers = 0;
+
         for (int i = 0; i < bank.size(); i++)
         {
+            int currLasers = 0;
             for (int j = 0; j < bank[i].length(); j++)
             {
                 if (bank[i][j] == '1')
                 {
-                    counter1++;
+                    currLasers++;
                 }
             }
-            if (counter1 != 0)
+
+            if (currLasers > 0)
             {
-                lasers += counter1 * counter2;
-                counter2 = counter1;
-                counter1 = 0;
+                output += (prevLasers * currLasers);
+                prevLasers = currLasers;
             }
         }
-        return lasers;
+
+        return output;
     }
 };
