1e5252fab6d9843be2b781027d0a8dfe5a5e2bde
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2024-01-04 14:46:56 +0100

    Time: 112 ms (83.43%), Space: 81.2 MB (97.61%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+
+        int output = 0;
+
+        int prev = nums[0];
+        int counter = 1;
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] != prev)
+            {
+                if (counter == 1)
+                {
+                    return -1;
+                }
+
+                if (counter % 3 == 0)
+                {
+                    output += counter / 3;
+                }
+                else if (counter % 3 == 1)
+                {
+                    output += (counter / 3)+1;
+                }
+                else if (counter % 3 == 2)
+                {
+                    output += (counter / 3)+1;
+                }
+
+                prev = nums[i];
+                counter = 1;
+            }
+            else
+            {
+                counter++;
+            }
+        }
+
+        if (counter == 1)
+        {
+            return -1;
+        }
+
+        if (counter % 3 == 0)
+        {
+            output += counter / 3;
+        }
+        else if (counter % 3 == 1)
+        {
+            output += (counter / 3)+1;
+        }
+        else if (counter % 3 == 2)
+        {
+            output += (counter / 3)+1;
+        }
+
+        return output;
+    }
+};
