906c9bf9d053e452ea6b02aefef00e05efd587f9
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2024-01-17 13:15:53 +0100

    Time: 2 ms (65.84%), Space: 8.5 MB (85.59%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        vector<int> nums(2000, 0);
+        sort(begin(arr),end(arr));
 
-        for (int i = 0; i < arr.size(); i++)
+        vector<int> frequencies;
+
+        int prev = arr[0];
+        int counter = 1;
+
+        for (int i = 1; i < arr.size(); i++)
         {
-            nums[arr[i]+1000]++;
-        }
-
-        sort(begin(nums), end(nums));
-
-        for (int i = 1; i < nums.size(); i++)
-        {
-            if (nums[i] != 0)
+            if (arr[i] != prev)
             {
-                if (nums[i] == nums[i-1])
-                {
-                    return false;
-                }
+                frequencies.push_back(counter);
+                prev = arr[i];
+                counter = 1;
+            }
+            else
+            {
+                counter++;
+            }
+        }
+        frequencies.push_back(counter);
+
+
+        sort(begin(frequencies), end(frequencies));
+
+        for (int i = 0; i < frequencies.size()-1; i++)
+        {
+            if (frequencies[i] == frequencies[i+1])
+            {
+                return false;
             }
         }
 
