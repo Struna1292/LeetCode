461aecfa9f3204f7a15357509af6734dab4aa078
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2024-02-15 13:11:10 +0100

    Time: 0 ms (100.00%), Space: 8.8 MB (25.20%) - LeetHub

---
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i < position.size(); i++)
+        {
+            if (position[i] % 2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+
+        if (odd > even)
+        {
+            return even;
+        }
+
+        return odd;
+    }
+};
