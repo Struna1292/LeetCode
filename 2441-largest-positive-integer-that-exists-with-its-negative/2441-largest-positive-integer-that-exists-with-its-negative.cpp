c9aba3c8dfd9bb4b3132dc19c8d2c41ce8b6f091
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2024-05-02 11:59:44 +0200

    Time: 11 ms (94.04%), Space: 22.8 MB (65.86%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,48 +1,35 @@
 class Solution {
 public:
-    bool binarySearch(vector<int>& nums, int target)
-    {
-        int left = 0;
-        int right = nums.size()-1;
-
-        while (left <= right)
-        {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target)
-            {
-                return true;
-            }
-            if (target > nums[mid])
-            {
-                left = mid + 1;
-            }
-            else
-            {
-                right = mid - 1;
-            }
-        }
-        return false;
-    }
-
     int findMaxK(vector<int>& nums) {
         
         sort(begin(nums), end(nums));
 
-        for (int i = 0; i < nums.size(); i++)
+        int i = 0;
+        int j = nums.size()-1;
+
+        while (i < j)
         {
-            if (nums[i] < 0)
-            {
-                if(binarySearch(nums, nums[i]*-1))
-                {
-                    return nums[i] * -1;
-                }
-            }
-            else
+            int curr = nums[i] * -1;
+
+            if (curr <= 0)
             {
                 break;
             }
+            else if (curr == nums[j])
+            {
+                return curr;
+            }
+            else if (curr > nums[j])
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+
         }
+
 
         return -1;
     }
