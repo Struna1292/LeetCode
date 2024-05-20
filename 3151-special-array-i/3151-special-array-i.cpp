5843c464a3b18b8af8dc8211948b3c636a7aa3cd
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2024-05-20 14:13:52 +0200

    Time: 3 ms (80.00%), Space: 26.8 MB (60.00%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if ((nums[i-1] % 2 == 0 && nums[i] % 2 == 0) || (nums[i-1] % 2 != 0 && nums[i] % 2 != 0))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
