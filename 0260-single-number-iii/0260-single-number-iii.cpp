22b12efec80a66a1ae3272249bb0704a905327ad
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2024-05-31 14:04:06 +0200

    Time: 9 ms (44.18%), Space: 13.3 MB (30.38%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        sort(begin(nums), end(nums));
+        
+        map<int,int> mp;
 
-        int counter = 1;
-        int prev = nums[0];
-
-        vector<int> answer;
+        mp.insert({nums[0], 1});
 
         for (int i = 1; i < nums.size(); i++)
         {
-            if (nums[i] != prev)
-            {
-                if (counter == 1)
-                {
-                    answer.push_back(prev);
-                }
-                prev = nums[i];
-                counter = 1;
-            }
-            else
-            {
-                counter++;
-            }
-        }
-        if (counter == 1)
-        {
-            answer.push_back(prev);
+            mp[nums[i]]++;
         }
 
-        return answer;
+        vector<int> output;
+
+        map<int, int>::iterator itr;
+
+        for (itr = mp.begin(); itr != mp.end(); ++itr) 
+        {
+            if (itr->second == 1)
+            {
+                output.push_back(itr->first);
+            }
+        }
+        
+        
+        return output;
     }
 };
