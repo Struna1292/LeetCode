6e678337e333fac7c4dfdd2944c39dc9f63419ab
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2024-06-03 15:03:14 +0200

    Time: 23 ms (20.38%), Space: 11.8 MB (83.42%) - LeetHub

---
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        
+        int j = 0;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == t[j])
+            {
+                j++;
+                if (j == t.length())
+                {
+                    return 0;
+                }
+            }
+        }
+
+
+        return t.length() - j;
+    }
+};
