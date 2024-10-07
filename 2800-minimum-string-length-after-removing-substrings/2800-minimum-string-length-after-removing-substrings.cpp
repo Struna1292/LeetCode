a801bc4009099d0e8ca6986c16e39955738f90b8
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2024-10-07 13:18:05 +0200

    Time: 3 ms (89.38%), Space: 11.6 MB (61.24%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minLength(string s) {
+        
+        stack<char> st;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (st.empty())
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if (s[i] == 'B' && st.top() == 'A')
+                {
+                    st.pop();
+                }
+                else if (s[i] == 'D' && st.top() == 'C')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(s[i]);
+                }
+            }
+        }
+
+        return st.size();
+    }
+};
