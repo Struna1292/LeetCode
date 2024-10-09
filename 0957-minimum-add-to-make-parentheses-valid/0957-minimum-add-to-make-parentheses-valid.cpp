23484b4d44133f312f42ce1e5e638f04df4752f4
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2024-10-09 12:49:49 +0200

    Time: 0 ms (100%), Space: 7.9 MB (32.94%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        
+        stack<char> st;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (!st.empty() && st.top() == '('  && s[i] == ')')
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(s[i]);
+            }
+        }
+
+        return st.size();
+    }
+};
