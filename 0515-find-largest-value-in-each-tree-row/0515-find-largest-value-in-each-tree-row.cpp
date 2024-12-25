58648bb89d282d62eb5cf6fb550be53303610a0c
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2024-12-25 23:00:51 +0100

    Time: 1 ms (19.67%), Space: 23.1 MB (7.42%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -11,36 +11,46 @@
  */
 class Solution {
 public:
-    void inOrder(TreeNode* root, int depth, vector<int>& output)
+
+    void inOrder(TreeNode* root, int depth, vector<vector<int>>& nums)
     {
         if (root == nullptr)
         {
             return;
         }
 
-        cout << root->val << " " << depth << endl;
-
-        if (output.size() <= depth)
+        if (nums.size() < depth)
         {
-            output.push_back(root->val);
-        }
-        else
-        {
-            if (output[depth] < root->val)
-            {
-                output[depth] = root->val;
-            }
+            nums.push_back({});
         }
 
-        inOrder(root->left, depth+1, output);
+        nums[depth-1].push_back(root->val);
 
-        inOrder(root->right, depth+1, output);
+        inOrder(root->left, depth+1, nums);
+
+        inOrder(root->right, depth+1, nums);
     }
 
     vector<int> largestValues(TreeNode* root) {
+        
+        vector<vector<int>> nums;
+
+        inOrder(root, 1, nums);
+
         vector<int> output;
 
-        inOrder(root, 0, output);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int curr = nums[i][0];
+            for (int j = 1; j < nums[i].size(); j++)
+            {
+                if (curr < nums[i][j])
+                {
+                    curr = nums[i][j];
+                }
+            }
+            output.push_back(curr);
+        }
 
         return output;
     }
