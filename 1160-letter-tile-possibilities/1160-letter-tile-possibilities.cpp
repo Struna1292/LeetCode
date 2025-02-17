580e3232a0698cff42f44c9a2a54e2cdd9950343
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2025-02-17 14:28:58 +0100

    Time: 134 ms (6.13%), Space: 26.8 MB (5.23%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void helper(string& tiles, vector<bool> used, set<string>& st, string curr)
+    {
+        for (int i = 0; i < tiles.length(); i++)
+        {
+            if (!used[i])
+            {
+                used[i] = true;
+                string c = curr;
+                c.push_back(tiles[i]);
+                st.insert(c);
+                helper(tiles, used, st, c);
+                used[i] = false;
+            }
+        }
+    }
+
+    int numTilePossibilities(string tiles) {
+        set<string> st;
+
+        vector<bool> used(tiles.length(), false);
+
+        helper(tiles, used, st, "");
+
+        return st.size();
+    }
+};
