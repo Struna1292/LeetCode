cf0a4e5f69444f578dbbaca7be2e9c72501c8a0e
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2025-03-24 13:52:26 +0100

    Time: 72 ms (48.98%), Space: 126.8 MB (35.2%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -4,50 +4,49 @@ public:
         
         sort(begin(meetings), end(meetings));
 
-        vector<vector<int>> meetings2;
-
-        meetings2.push_back({meetings[0][0], meetings[0][1]});
+        vector<vector<int>> merged{{meetings[0][0], meetings[0][1]}};
 
         for (int i = 1; i < meetings.size(); i++)
         {
-            bool match = true;
+            int start = merged[merged.size()-1][0];
+            int end = merged[merged.size()-1][1];
 
-            if (meetings2[meetings2.size()-1][0] <= meetings[i][0] && meetings2[meetings2.size()-1][1] >= meetings[i][1])
+            if (start == meetings[i][0] && end == meetings[i][1])
             {
-                match = false;
+                continue;
             }
-            else if (meetings2[meetings2.size()-1][0] <= meetings[i][0] && meetings2[meetings2.size()-1][1] >= meetings[i][0])
+            else if (start == meetings[i][0] && end < meetings[i][1])
             {
-                meetings2[meetings2.size()-1][1] = meetings[i][1];
-                match = false;
+                merged[merged.size()-1][1] = meetings[i][1];
             }
-            
-
-            if (match)
+            else if (end >= meetings[i][0] || end+1 == meetings[i][0])
             {
-                meetings2.push_back(meetings[i]);
+                if (end < meetings[i][1])
+                {
+                    merged[merged.size()-1][1] = meetings[i][1];
+                }
             }
-        }
-
-
-        int prev = 1;
-
-        int output = 0;
-
-        for (int i = 0; i < meetings2.size(); i++)
-        {
-            if (prev != meetings2[i][0] && i != 0)
+            else if (end >= days)
             {
-                output += meetings2[i][0] - prev - 1;
+                merged[merged.size()-1][1] = days;
+                break;
+            }
+            else if (meetings[i][0] > days)
+            {
+                break;
             }
             else
             {
-                output += meetings2[i][0] - prev;
+                merged.push_back(meetings[i]);
             }
-            prev = meetings2[i][1];
         }
 
-        output += days - prev;
+        int output = days;
+
+        for (int i = 0; i < merged.size(); i++)
+        {
+            output -= (merged[i][1] - merged[i][0])+1;
+        }
 
         return output;
     }
