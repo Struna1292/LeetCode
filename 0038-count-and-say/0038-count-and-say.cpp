530858b0bd3397c2c5b33fac5bc5013e3c231fc6
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2025-04-18 13:49:40 +0200

    Time: 2 ms (89.33%), Space: 9.6 MB (89.27%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
-    string countAndSay(int n) {
+    string RLE(string s)
+    {
+        if (s == "")
+        {
+            return "";
+        }
 
+        int counter = 1;
+        char prev = s[0];
         string output;
-        if (n == 1)
-        {
-            return "1";
-        }
-        else
-        {
-            output = "11";
-        }
-        int counter = 0;
-        for (int i = 2; i < n; i++)
-        {
-            string curr;
-            counter = 1;
-            char prev = output[0];
-            for (int j = 1; j < output.size(); j++)
-            {
-                if (output[j] != prev)
-                {
-                    curr.push_back(counter + '0');
-                    curr.push_back(prev);
-                    counter = 1;
-                }
-                else
-                {
-                    counter++;
-                }
-                if (j+1 == output.size())
-                {
-                    curr.push_back(counter + '0');
-                    curr.push_back(output[j]);
-                }
-                prev = output[j];
-            }
 
-            output = curr;
+        for (int i = 1; i < s.length(); i++)
+        {
+            if (s[i] != prev)
+            {
+                output.push_back('0'+counter);
+                output.push_back(prev);
+
+                counter = 1;
+                prev = s[i];
+            }
+            else
+            {
+                counter++;
+            }
         }
+        output.push_back('0'+counter);
+        output.push_back(prev);
+
+        return output;
+    }
+
+    string countAndSay(int n) {
+        
+        string output = "1";
+
+        for (int i = 1; i < n; i++)
+        {
+            output = RLE(output);
+        }
+
         return output;
     }
 };
