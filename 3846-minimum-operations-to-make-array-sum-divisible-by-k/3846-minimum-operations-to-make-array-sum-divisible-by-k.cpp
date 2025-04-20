16e94b1150cea91323b7041494babc98b5c38057
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2025-04-20 22:38:43 +0200

    Time: 0 ms (100%), Space: 45.2 MB (72.37%) - LeetHub

---
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/3846-minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int sum = 0;
+        for (int n : nums)
+        {
+            sum += n;
+        }
+
+        return sum % k;
+    }
+};
