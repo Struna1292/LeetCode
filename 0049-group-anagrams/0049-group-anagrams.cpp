e426e855d16f5ab892403b4e0630372a6d09a8a8
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2025-06-29 15:05:44 +0200

    Time: 24 ms (29.91%), Space: 25.1 MB (62.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,44 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        struct Anagram {
-            string sorted;
-            vector<int> indexes;
-        };
+        
+        map<string,vector<string>> mp;
 
-        vector<Anagram> anagrams;
-
-        for (int i = 0; i < strs.size(); i++)
-        {
-            string check = strs[i];
-            sort(begin(check), end(check));
-            int counter = 0;
-            for (int j = 0; j < anagrams.size(); j++)
-            {
-                if (anagrams[j].sorted == check)
-                {
-                    anagrams[j].indexes.push_back(i);
-                    break;
-                }
-                counter++;
-            }
-
-            if (counter == anagrams.size())
-            {
-                anagrams.push_back({check, {i}});
-            }
+        for (int i = 0; i < strs.size(); i++) {
+            string curr = strs[i];
+            sort(begin(curr), end(curr));
+            mp[curr].push_back(strs[i]);
         }
 
         vector<vector<string>> output;
 
-        for (int i = 0; i < anagrams.size(); i++)
-        {
-            vector<string> curr;
-            for (int j = 0; j < anagrams[i].indexes.size(); j++)
-            {
-                curr.push_back(strs[anagrams[i].indexes[j]]);
-            }
-            output.push_back(curr);
+        for (auto itr = mp.begin(); itr != mp.end(); itr++) {
+            output.push_back(itr->second);
         }
 
         return output;
