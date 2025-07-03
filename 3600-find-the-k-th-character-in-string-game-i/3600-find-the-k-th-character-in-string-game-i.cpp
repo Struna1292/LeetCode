c6bfc38d9da77b8b9b1507921d6eb235078e80d6
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2025-07-03 21:31:02 +0200

    Time: 2 ms (64.66%), Space: 9.4 MB (68.53%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        
         string s = "a";
 
-        while (s.length() < k)
-        {
+        while (s.length() < k) {
             string curr = s;
 
-            for (int i = 0; i < curr.length(); i++)
-            {
-                if (curr[i]+1 > 'z')
-                {
+            for (int i = 0; i < curr.length(); i++) {
+                if (curr[i]+1 > 'z') {
                     curr[i] = 'a';
-                }
-                else
-                {
+                } else {
                     curr[i] += 1;
                 }
             }
