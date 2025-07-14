c7b70b2a1c4fb70e4ce7562fda4be8fb661bd98d
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2025-07-14 08:25:45 +0200

    Time: 15 ms (22.23%), Space: 104.8 MB (23.02%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
+    void DFS(int i, int j, vector<vector<int>>& grid, vector<vector<bool>>& seen, int& perimeter) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size() || grid[i][j] == 0) {
+            perimeter++; 
+            return;
+        }
+
+        if (seen[i][j]) {
+            return;
+        }
+
+        seen[i][j] = true;
+
+        DFS(i+1, j, grid, seen, perimeter);
+        DFS(i-1, j, grid, seen, perimeter);
+        DFS(i, j+1, grid, seen, perimeter);
+        DFS(i, j-1, grid, seen, perimeter);
+    }
+
     int islandPerimeter(vector<vector<int>>& grid) {
-        
-        int output = 0;
+        // notice that with DFS when we go to invalid coordinates that makes perimeter++
 
-        for (int i = 0; i < grid.size(); i++)
-        {
-            int prev = 0;
-            for (int j = 0; j < grid[i].size(); j++)
-            {
-                if (grid[i][j] == 1 && prev != 1)
-                {
-                    output += 2;
+        vector<vector<bool>> seen(grid.size(), vector<bool>(grid[0].size(), false));
+
+        int perimeter = 0;
+
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == 1 && !seen[i][j]) {
+                    DFS(i, j, grid, seen, perimeter);
                 }
-                prev = grid[i][j];
             }
         }
 
-        for (int i = 0; i < grid[0].size(); i++)
-        {
-            int prev = 0;
-            for (int j = 0; j < grid.size(); j++)
-            {
-                if (grid[j][i] == 1 && prev != 1)
-                {
-                    output += 2;
-                }
-                prev = grid[j][i];
-            }
-        }
-
-        return output;
+        return perimeter;
     }
 };
