593d4c17beb32ae4bed99867c0c63dc17cef26e0
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2025-08-25 17:04:36 +0200

    Time: 27 ms (27.59%), Space: 31.5 MB (10.87%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -2,72 +2,77 @@ class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         
-        vector<int> output;
-
-        int counter = 2;
-
-        for (int i = 0; i < mat.size(); i++)
-        {
-            int row = i;
-            int column = 0;
-
+        vector<vector<int>> leftDiagonals;
+        // diagonals starting from first column
+        for (int i = 0; i < mat.size(); i+=2) {
             vector<int> curr;
-
-            while (row >= 0 && column < mat[0].size())
-            {
-                curr.push_back(mat[row][column]);
-                row--;
-                column++;
+            for (int j = 0; j < mat[i].size() && (i-j) >= 0; j++) {
+                curr.push_back(mat[i-j][j]);
             }
 
-            if (counter % 2 == 0)
-            {
-                for (int j = 0; j < curr.size(); j++)
-                {
-                    output.push_back(curr[j]);
-                }
-            }
-            else
-            {
-                for (int j = curr.size()-1; j >= 0; j--)
-                {
-                    output.push_back(curr[j]);
-                }
-            }
-            
-            counter++;
+            leftDiagonals.push_back(curr);
         }
 
-        for (int i = 1; i < mat[0].size(); i++)
-        {
-            int row = mat.size()-1;
-            int column = i;
-
+        // diagonals starting from last row
+        int index = 2; 
+        if (mat.size() % 2 == 0) {
+            index = 1;
+        }
+        for (int i = index; i < mat[mat.size()-1].size(); i+=2) {
             vector<int> curr;
-
-            while (row >= 0 && column < mat[0].size())
-            {
-                curr.push_back(mat[row][column]);
-                row--;
-                column++;
+            int columnIndex = i;
+            for (int j = mat.size()-1; j >= 0 && columnIndex < mat[j].size(); j--) {
+                curr.push_back(mat[j][columnIndex]);
+                columnIndex++;
             }
 
-            if (counter % 2 == 0)
-            {
-                for (int j = 0; j < curr.size(); j++)
-                {
-                    output.push_back(curr[j]);
+            leftDiagonals.push_back(curr);
+        }
+
+        vector<vector<int>> rightDiagonals;
+        // diagonals starting from first row
+        for (int i = 1; i < mat[0].size(); i+=2) {
+            vector<int> curr;
+            for (int j = 0; j < mat.size() && (i-j) >= 0; j++) {
+                curr.push_back(mat[j][i-j]);
+            }
+
+            rightDiagonals.push_back(curr);
+        }
+
+        // diagonals starting from last column
+        index = 1;
+        if (mat[0].size() % 2 == 0) {
+            index = 2;
+        }
+        for (int i = index; i < mat.size(); i+=2) {
+            vector<int> curr;
+            int currIndex = i;
+            for (int j = mat[0].size()-1; j >= 0 && currIndex < mat.size(); j--) {
+                curr.push_back(mat[currIndex][j]);
+                currIndex++;
+            }
+
+            rightDiagonals.push_back(curr);
+        }
+
+        vector<int> output;
+
+        int i = 0;
+        while (i < leftDiagonals.size() || i < rightDiagonals.size()) {
+            if (i < leftDiagonals.size()) {
+                for (int j = 0; j < leftDiagonals[i].size(); j++) {
+                    output.push_back(leftDiagonals[i][j]);
                 }
             }
-            else
-            {
-                for (int j = curr.size()-1; j >= 0; j--)
-                {
-                    output.push_back(curr[j]);
+
+            if (i < rightDiagonals.size()) {
+                for (int j = 0; j < rightDiagonals[i].size(); j++) {
+                    output.push_back(rightDiagonals[i][j]);
                 }
             }
-            
-            counter++;
+
+            i++;
         }
 
         return output;
