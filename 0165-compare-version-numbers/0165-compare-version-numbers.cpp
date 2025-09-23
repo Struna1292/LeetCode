51a667154a7210d6e000d54777d5f1e5f28ec001
Struna1292 <105943844+Struna1292@users.noreply.github.com>
2025-09-23 15:23:30 +0200

    Time: 0 ms (100%), Space: 8.6 MB (30.92%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,61 +1,45 @@
 class Solution {
 public:
-    vector<int> convertVersionToVector(string version)
-    {
-        vector<int> v;
-
-        string curr;
-
-        for (int i = 0; i < version.length(); i++)
-        {
-            if (version[i] != '.')
-            {
-                curr.push_back(version[i]);
-            }
-            else
-            {
-                int num = stoi(curr);
-                v.push_back(stoi(curr));
+    vector<int> convertToVector(string& version) {
+        vector<int> nums;
+        string curr = "";
+        for (int i = 0; i < version.length(); i++) {
+            if (version[i] == '.') {
+                nums.push_back(stoi(curr));
                 curr = "";
             }
+            else {
+                curr.push_back(version[i]);
+            }
         }
-        v.push_back(stoi(curr));
+        nums.push_back(stoi(curr));
 
-        return v;
+        return nums;        
+    }
+
+    void fixSizes(vector<int>& nums1, vector<int>& nums2) {
+        while (nums1.size() < nums2.size()) {
+            nums1.push_back(0);
+        }
+
+        while (nums2.size() < nums1.size()) {
+            nums2.push_back(0);
+        }        
     }
 
     int compareVersion(string version1, string version2) {
         
-        vector<int> v1 = convertVersionToVector(version1);
-        vector<int> v2 = convertVersionToVector(version2);
+        vector<int> nums1 = convertToVector(version1);
+        vector<int> nums2 = convertToVector(version2);
 
+        fixSizes(nums1, nums2);
 
-        while (v1.size() > v2.size())
-        {
-            v2.push_back({0});
-        }
-
-        while (v2.size() > v1.size())
-        {
-            v1.push_back({0});
-        }
-
-
-        int i = 0;
-
-        while (i < v1.size())
-        {
-            if (v1[i] < v2[i])
-            {
-                return -1;
-            }
-            else if (v1[i] > v2[i])
-            {
+        for (int i = 0; i < nums1.size(); i++) {
+            if (nums1[i] > nums2[i]) {
                 return 1;
             }
-            else
-            {
-                i++;
+            else if (nums1[i] < nums2[i]) {
+                return -1;
             }
         }
 
