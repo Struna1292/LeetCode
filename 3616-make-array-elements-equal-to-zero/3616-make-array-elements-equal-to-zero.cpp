1cae2d17059d063d8f0a1c4705908467b5fc7cc3
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2025-10-28 20:42:01 +0100

    Time: 4 ms (68.57%), Space: 22.9 MB (28.57%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
@@ -4,38 +4,30 @@ public:
 
         vector<int> prefix(nums.size(), 0);
         int curr = 0;
-        for (int i = 0; i < nums.size(); i++)
-        {
+        for (int i = 0; i < nums.size(); i++) {
             curr += nums[i];
             prefix[i] = curr;
         }
 
         vector<int> suffix(nums.size(), 0);
         curr = 0;
-        for (int i = nums.size()-1; i >= 0; i--)
-        {
+        for (int i = nums.size()-1; i >= 0; i--) {
             curr += nums[i];
             suffix[i] = curr;
-            
         }
-        
+
         int output = 0;
 
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] == 0)
-            {
-                if (prefix[i] == suffix[i])
-                {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                if (prefix[i] == suffix[i]) {
                     output += 2;
                 }
-                else if (abs(prefix[i] - suffix[i]) == 1)
-                {
+                else if (abs(prefix[i] - suffix[i]) == 1) {
                     output++;
-                }
+                }                
             }
-        }
-
+        }       
 
         return output;
     }
