12c1c366997ea250b3a18c54376c2e0ea83f1a5a
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2025-10-29 13:34:05 +0100

    Time: 0 ms (100%), Space: 8.1 MB (99.74%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        
         int curr = 1;
 
-        while (curr < n)
-        {
+        while (curr < n) {
             curr <<= 1;
             curr |= 1;
         }
