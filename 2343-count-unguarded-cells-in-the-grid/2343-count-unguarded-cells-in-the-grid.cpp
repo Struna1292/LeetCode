2324c1eeb1ab114860376ec27496555c9777893b
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2025-11-02 20:45:53 +0100

    Time: 59 ms (67.62%), Space: 167.8 MB (88.1%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
@@ -1,89 +1,75 @@
 class Solution {
 public:
-    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        sort(begin(guards),end(guards));
-        sort(begin(walls), end(walls));
-
-        vector<vector<int>> grid(m, vector<int>(n, 0));
-
-        // 0 - empty space
-        // 1 - guarded space
-        // 2 - guard
-        // 3 - wall
-
-        int currGuards = 0;
-        int currWalls = 0;
-
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (currGuards < guards.size() && guards[currGuards][0] == i && guards[currGuards][1] == j)
-                {
-                    grid[i][j] = 2; 
-                    currGuards++;
-                }
-
-                if (currWalls < walls.size() && walls[currWalls][0] == i && walls[currWalls][1] == j)
-                {
-                    grid[i][j] = 3;
-                    currWalls++;
-                }
+    void fillLeft(int i, int j, vector<vector<char>>& grid) {
+        for (; j >= 0; j--) {
+            if (grid[i][j] == 'W' || grid[i][j] == 'G') {
+                return;
             }
+
+            grid[i][j] = '-';
+        }
+    }
+
+    void fillRight(int i, int j, vector<vector<char>>& grid) {
+        for (; j < grid[i].size(); j++) {
+            if (grid[i][j] == 'W' || grid[i][j] == 'G') {
+                return;
+            }
+
+            grid[i][j] = '-';
+        }
+    }
+
+    void fillUp(int i, int j, vector<vector<char>>& grid) {
+        for (; i >= 0; i--) {
+            if (grid[i][j] == 'W' || grid[i][j] == 'G') {
+                return;
+            }
+
+            grid[i][j] = '-';
+        }        
+    }
+
+    void fillDown(int i, int j, vector<vector<char>>& grid) {
+        for (; i < grid.size(); i++) {
+            if (grid[i][j] == 'W' || grid[i][j] == 'G') {
+                return;
+            }
+
+            grid[i][j] = '-';
+        }
+    }
+
+    void fillGrid(int i, int j, vector<vector<char>>& grid) {
+        fillLeft(i, j-1, grid);
+        fillRight(i, j+1, grid);
+        fillUp(i-1, j, grid);
+        fillDown(i+1, j, grid);
+    }
+
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        vector<vector<char>> grid(m, vector<char>(n, ' '));
+
+        for (int i = 0; i < walls.size(); i++) {
+            grid[walls[i][0]][walls[i][1]] = 'W';
         }
 
+        for (int i = 0; i < guards.size(); i++) {
+            grid[guards[i][0]][guards[i][1]] = 'G';
+        }
 
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (grid[i][j] == 2)
-                {
-                    //check up
-                    int k = i-1;
-                    while (k >= 0 && grid[k][j] != 3 && grid[k][j] != 2)
-                    {
-                        grid[k][j] = 1;
-                        k--;
-                    }
-
-                    //check right
-                    k = j+1;
-                    while (k < n && grid[i][k] != 3 && grid[i][k] != 2)
-                    {
-                        grid[i][k] = 1;
-                        k++;
-                    }
-
-
-                    //check down
-                    k = i+1;
-                    while (k < m && grid[k][j] != 3 && grid[k][j] != 2)
-                    {
-                        grid[k][j] = 1;
-                        k++;
-                    }
-
-
-                    //check left
-                    k = j-1;
-                    while (k >= 0 && grid[i][k] != 3 && grid[i][k] != 2)
-                    {
-                        grid[i][k] = 1;
-                        k--;
-                    }
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == 'G') {
+                    fillGrid(i, j, grid);
                 }
             }
         }
 
         int output = 0;
-
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (grid[i][j] == 0)
-                {
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == ' ') {
                     output++;
                 }
             }
