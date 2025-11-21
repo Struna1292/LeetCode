7176ef048bd95dc971aad3c1396ef2856fc81263
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2025-11-21 13:13:36 +0100

    Time: 49 ms (91.69%), Space: 16.3 MB (29.44%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        
-        set<string> st;
+        // pair represents first and last index
+        unordered_map<char, pair<int, int>> indexes;
 
-        vector<vector<int>> indexes(26, vector<int>());
-
-        for (int i = 0; i < s.length(); i++)
-        {
-            if (indexes[s[i]-'a'].size() >= 1)
-            {
-                int index = indexes[s[i]-'a'][indexes[s[i]-'a'].size()-1] + 1;
-                for (int j = index; j < i; j++)
-                {
-                    string curr;
-                    curr.push_back(s[i]);
-                    curr.push_back(s[j]);
-                    curr.push_back(s[i]);
-                    st.insert(curr);
-                }
-
-                if (indexes[s[i]-'a'].size() == 2)
-                {
-                    string curr;
-                    curr.push_back(s[i]);
-                    curr.push_back(s[i]);
-                    curr.push_back(s[i]);
-                    st.insert(curr);
-                }
+        for (int i = 0; i < s.length(); i++) {
+            if (!indexes.contains(s[i])) {
+                indexes[s[i]] = { i, -1 };
             }
-
-            indexes[s[i]-'a'].push_back(i);
+            else {
+                indexes[s[i]].second = i;
+            }
         }
 
-        return st.size();
+        unordered_map<char, int> um;
+        unordered_map<char, unordered_map<char, int>> prev;
+        int output = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+            if (indexes[s[i]].first == i) {
+                prev[s[i]] = um;
+            }
+            else if (indexes[s[i]].second == i) {
+
+                for (auto itr = um.begin(); itr != um.end(); itr++) {
+                    char curr = itr->first;
+                    int occs = itr->second;
+
+                    if ((curr == s[i] && occs - prev[s[i]][curr] > 1) || (curr != s[i] && occs - prev[s[i]][curr] > 0)) {
+                        output++;
+                    }
+                }
+
+            }
+
+            um[s[i]]++;
+        }
+
+        return output;
     }
 };
