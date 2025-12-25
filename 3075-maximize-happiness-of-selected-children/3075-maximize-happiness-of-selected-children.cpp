ef83d8104539b4ffcaf78cd33ed3c57abb5a5f3e
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2025-12-25 12:28:40 +0100

    Time: 160 ms (49.65%), Space: 107.8 MB (84.75%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -4,17 +4,13 @@ public:
         sort(begin(happiness), end(happiness), greater<int>());
 
         long long output = 0;
-
-        for (int i = 0; i < k; i++)
-        {
-            if ((happiness[i] - i) > 0)
-            {
-                output += (happiness[i] - i);
-            }
-            else
-            {
+        for (int i = 0; i < k; i++) {
+            int curr = happiness[i] - i;
+            if (curr <= 0) {
                 break;
             }
+
+            output += curr;
         }
 
         return output;
