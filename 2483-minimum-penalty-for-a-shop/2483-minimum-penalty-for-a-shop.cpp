5301f89d299c7bbf36022d71ce249e29e42fc5ca
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2025-12-26 11:05:21 +0100

    Time: 11 ms (37.38%), Space: 17.7 MB (53.22%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-
-        vector<int> leftN(customers.length()+1, 0);
-        vector<int> rightY(customers.length()+1, 0);
-        int counterN = 0;
-        int counterY = 0;
+        vector<int> prefix(customers.length()+1, 0);
         
-        int j = customers.length()-1;
-        int i = 0;
-        for ( i = 0; i < customers.length(); i++)
-        {
-            leftN[i] = counterN;
-            rightY[j+1] = counterY;
-            if (customers[i] == 'N')
-                counterN++;
+        int curr = 0;
+        for (int i = 0; i < customers.length(); i++) {
+            if (customers[i] == 'Y') {
+                curr++;
+            }
 
-            if (customers[j] == 'Y')
-                counterY++;
-
-            j--;
+            prefix[i+1] = curr;
         }
-        leftN[i] = counterN;
-        rightY[j+1] = counterY;
 
-        int index = 0;
-        int smallest = leftN[0] + rightY[0];
-        for (int i = 1; i < customers.length()+1; i++)
-        {
-            if (leftN[i] + rightY[i] < smallest)
-            {
-                smallest = leftN[i] + rightY[i];
-                index = i;
+        int currMissedHours = 0;
+
+        int bestHour = -1;
+        int bestPenalty = 0;
+        for (int i = 0; i < prefix.size(); i++) {
+            if (i > 0 && customers[i-1] == 'N') {
+                currMissedHours++;
+            }
+
+            int currPenalty = (curr - prefix[i]) + currMissedHours;
+            if (bestHour == -1 || currPenalty < bestPenalty) {
+                bestHour = i;
+                bestPenalty = currPenalty;
             }
         }
 
-        return index;
+        return bestHour;
     }
 };
