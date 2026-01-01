1da6eda5f387b6242cb317da4afedc2b041d894a
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2026-01-01 13:10:44 +0100

    Time: 0 ms (100%), Space: 11.4 MB (77.72%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size()-1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        digits[0] = 1;
+        digits.push_back(0);
+
+        return digits;
+    }
+};
