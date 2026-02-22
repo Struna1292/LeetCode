6ea2a73d399c5159c0b5ad5840b4ec830a01e062
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2026-02-22 13:16:02 +0100

    Time: 0 ms (100%), Space: 7.9 MB (70.75%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int output = 0;
+        int counter = 0;
+        int ones = 0;
+
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                if (counter > output && ones >= 1) {
+                    output = counter;
+                }
+
+                counter = 0;
+                ones++;
+            }
+            else {
+                counter++;
+            }  
+            n >>= 1;
+        }
+
+        if (ones <= 1) {
+            return 0;
+        }
+
+        return output+1;
+    }
+};
