80af928bd428a95662f5de94ecec2750e9d67560
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2026-02-26 17:31:09 +0100

    Time: 10 ms (8.29%), Space: 8.8 MB (83.16%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,77 +1,53 @@
 class Solution {
 public:
-    void divideByTwo(string& s)
-    {
-        char prev = '0';
-
-        for (int i = 0; i < s.length(); i++)
-        {
-            if (s[i] == '1' && prev == '0')
-            {
+    void addOne(string& s) {
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s[i] == '1') {
                 s[i] = '0';
-                prev = '1';
             }
-            else if (s[i] == '0' && prev == '1')
-            {
-                s[i] = '1';
-                prev = '0';
-            }
-        }
-    }
-
-    void addOne(string& s)
-    {   
-        for (int i = s.length()-1; i >= 0; i--)
-        {
-            if (s[i] == '0')
-            {
+            else {
                 s[i] = '1';
                 return;
             }
-            else
-            {
-                s[i] = '0';
+        }
+
+        s.insert(s.begin(), '1');
+    }
+
+    void divideByTwo(string& s) {
+        for (int i = s.length()-1; i > 0; i--) {
+            s[i] = s[i-1];
+        }
+        s[0] = '0';
+    }
+
+    bool isOne(string& s) {
+        for (int i = 0; i < s.length()-1; i++) {
+            if (s[i] == '1') {
+                return false;
             }
         }
 
-        s.push_back('0');
-        s[0] = '1';
+        return s[s.length()-1] == '1';
     }
 
     int numSteps(string s) {
-        
-        int counter = 0;
-
-        while (true)
-        {
-            if (s[s.length()-1] == '1')
-            {
-                bool found = false;
-
-                for (int i = 0; i < s.length()-1; i++)
-                {
-                    if (s[i] == '1')
-                    {
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (!found)
-                {
-                    break;
-                }
-
-                addOne(s);
-            }
-            else
-            {
-                divideByTwo(s);
-            }
-
-            counter++;
+        if (s == "1") {
+            return 0;
         }
 
-        return counter;
+        int output = 0;
+        
+        while (!isOne(s)) {
+            if (s[s.length()-1] == '1') {
+                addOne(s);
+            }
+            else {
+                divideByTwo(s);
+            }
+            output++;
+        }
+        
+        return output;
     }
 };
