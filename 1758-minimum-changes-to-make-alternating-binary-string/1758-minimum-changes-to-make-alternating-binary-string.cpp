ef19aee4706e91c370f9e9dd1ef167aee2cf31d0
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2026-03-05 11:38:09 +0100

    Time: 0 ms (100%), Space: 9.5 MB (20.96%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-    int minOperations(string s) {  
+    int minOperations(string s) {
         
-        int counter1 = 0; // sequence starting with 1
-        int counter2 = 0; // sequence starting with 0
-
-        for (int i = 0; i < s.length(); i++)
-        {
-            if (s[i] == '1')
-            {
-                if ((i+1) % 2 == 1)
-                {
-                    counter2++;
+        int startsZero = 0;
+        int startsOne = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                if (s[i] == '0') {
+                    startsOne++;
                 }
-                else
-                {
-                    counter1++;
+                else {
+                    startsZero++;
                 }
             }
-            else
-            {
-                if ((i+1) % 2 == 1)
-                {
-                    counter1++;
+            else {
+                if (s[i] == '0') {
+                    startsZero++;
                 }
-                else
-                {
-                    counter2++;
+                else {
+                    startsOne++;
                 }
             }
         }
-        
-        if (counter1 < counter2)
-        {
-            return counter1;
-        }
 
-        return counter2;
+        return min(startsZero, startsOne);
     }
 };
