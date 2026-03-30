95765cecc0947d0aae36539c97a5f61aa90ca7c1
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2026-03-30 17:08:15 +0200

    Time: 17 ms (43.68%), Space: 20.8 MB (41.38%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -3,34 +3,27 @@ public:
     bool checkStrings(string s1, string s2) {
         unordered_map<char, int> even;
         unordered_map<char, int> odd;
-        
+
         for (int i = 0; i < s1.length(); i++) {
             if (i % 2 == 0) {
                 even[s1[i]]++;
+                even[s2[i]]--;
             }
             else {
                 odd[s1[i]]++;
+                odd[s2[i]]--;
             }
         }
 
-        for (int i = 0; i < s1.length(); i++) {
-            char c = s2[i];
-
-            if (i % 2 == 0) {
-                if (even[c] > 0) {
-                    even[c]--;
-                }
-                else {
-                    return false;
-                }
+        for (auto itr = even.begin(); itr != even.end(); itr++) {
+            if (itr->second != 0) {
+                return false;
             }
-            else {
-                if (odd[c] > 0) {
-                    odd[c]--;
-                }
-                else {
-                    return false;
-                }
+        }
+
+        for (auto itr = odd.begin(); itr != odd.end(); itr++) {
+            if (itr->second != 0) {
+                return false;
             }
         }
 
