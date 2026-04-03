d6737644e8ac50d4730fa885470a82d693b18a9b
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2026-04-03 14:48:48 +0200

    Time: 0 ms (100%), Space: 9.1 MB (99.67%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,10 +1,50 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
+        if (str1.length() > str2.length()) {
+            swap(str1, str2);
+        }
 
-        if (str1+str2 == str2+str1)
-        {
-            return str1.substr(0, gcd(str1.length(), str2.length()));
+        for (int length = str1.length(); length > 0; length--) {
+            if (str1.length() % length != 0 || str2.length() % length != 0) {
+                continue;
+            }
+            
+            bool matching = true;
+            for (int startIndex = 0; startIndex + length <= str2.length(); startIndex += length) {
+                if (!matching) {
+                    break;
+                }
+
+                for (int j = 0; j < length; j++) {
+                    if (str1[j] != str2[startIndex+j]) {
+                        matching = false;
+                        break;
+                    }
+                }
+            }
+
+            for (int startIndex = 0; startIndex + length <= str1.length(); startIndex += length) {
+                if (!matching) {
+                    break;
+                }
+
+                for (int j = 0; j < length; j++) {
+                    if (str1[j] != str1[startIndex+j]) {
+                        matching = false;
+                        break;
+                    }
+                }
+            }
+
+            if (matching) {
+                string output;
+
+                for (int i = 0; i < length; i++) {
+                    output.push_back(str1[i]);
+                }
+                return output;
+            }
         }
 
         return "";
