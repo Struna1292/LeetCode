9af6b648195a8cff6631c274fee8db621190c6d2
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2026-05-26 05:37:22 +0200

    Time: 0 ms (100%), Space: 9.5 MB (35.03%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
+        unordered_set<char> us;
 
-        vector<int> lowerCase(26, 0);
-        vector<int> upperCase(26, 0);
-
-
-        for (int i = 0; i < word.length(); i++)
-        {
-            if (word[i] >= 65 && word[i] <= 90)
-            {
-                upperCase[word[i]-'A']++;
-            }
-            else
-            {
-                lowerCase[word[i]-'a']++;
-            }
+        for (int i = 0; i < word.length(); i++) {
+            us.insert(word[i]);
         }
 
         int output = 0;
+        for (int i = 0; i < 26; i++) {
+            char lowerCase = 'a' + i;
+            char upperCase = 'A' + i;
 
-        for (int i = 0; i < 26; i++)
-        {
-            if (upperCase[i] > 0 && lowerCase[i] > 0)
-            {
+            if (us.contains(lowerCase) && us.contains(upperCase)) {
                 output++;
             }
         }
