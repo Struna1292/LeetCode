b1ddc4c6caeaddea2bdde945dcbbcbe0477d79be
Kacper Kowalczyk <kacperkowalczyk122@o2.pl>
2026-05-27 14:27:12 +0200

    Time: 114 ms (16.01%), Space: 25.6 MB (23.31%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
+        unordered_set<char> us;
+        unordered_set<char> failed;
 
-        vector<vector<int>> lowerCase(26, vector<int>());
-        vector<vector<int>> upperCase(26, vector<int>());
+        for (int i = 0; i < word.length(); i++) {
+            if (word[i] >= 'a' && word[i] <= 'z') {
+                char upperCase = 'A' + (word[i] - 'a');
+                if (us.contains(upperCase)) {
+                    failed.insert(word[i]);
+                }
+            }
 
-        for (int i = 0; i < word.length(); i++)
-        {
-            if (word[i] >= 65 && word[i] <= 90)
-            {
-                upperCase[word[i]-'A'].push_back(i);
-            }
-            else
-            {
-                lowerCase[word[i]-'a'].push_back(i);
-            }
+            us.insert(word[i]);
         }
 
         int output = 0;
+        for (int i = 0; i < 26; i++) {
+            char lowerCase = 'a' + i;
+            char upperCase = 'A' + i;
 
-        for (int i = 0; i < 26; i++)
-        {
-            if (lowerCase[i].size() >= 1 && upperCase[i].size() >= 1 && lowerCase[i][lowerCase[i].size()-1] < upperCase[i][0])
-            {
+            if (!failed.contains(lowerCase) && us.contains(lowerCase) && us.contains(upperCase)) {
                 output++;
             }
         }
